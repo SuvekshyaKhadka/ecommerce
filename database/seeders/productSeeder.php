6a858a5e76aea1a8ce607d3e2ec5120f4b552ad4
tinakhadka <tinakhadka567@gmail.com>
2025-04-25 13:58:59 +0545

    product table added

--- database/seeders/productSeeder.php
+++ database/seeders/productSeeder.php
@@ -14,12 +14,48 @@ class productSeeder extends Seeder
     public function run(): void
     {
         //
+    //     DB::table('product')->insert([
+    //     'name'=>'LG mobile',
+    //     "price"=>'10,000',
+    //     "description"=>'this is the lg 8gb mobile phone with advanced featurs and sleek design',
+    //     "category"=>'mobile',
+    //     "gallery"=>"https://images.pexels.com/photos/15315956/pexels-photo-15315956.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=500&w=1400&dpr=1"
+    // ]);
+    // this for the single database input into the database for the seeding
+
         DB::table('product')->insert([
-        'name'=>'LG mobile',
-        "price"=>'10,000',
-        "description"=>'this is the lg 8gb mobile phone with advanced featurs and sleek design',
-        "category"=>'mobile',
-        "gallery"=>"https://images.pexels.com/photos/15315956/pexels-photo-15315956.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=500&w=1400&dpr=1"
-    ]);
+            [
+                'name'=>'LG mobile',
+                "price"=>'10,000',
+                "description"=>'this is the lg 8gb mobile phone with advanced featurs and sleek design',
+                "category"=>'mobile',
+                "gallery"=>"https://images.pexels.com/photos/15315956/pexels-photo-15315956.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=500&w=1400&dpr=1"
+
+            ],
+            [
+            'name'=>'LG mobile',
+            "price"=>'10,000',
+            "description"=>'this is the lg 8gb mobile phone with advanced featurs and sleek design',
+            "category"=>'mobile',
+            "gallery"=>"https://images.pexels.com/photos/15315956/pexels-photo-15315956.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=500&w=1400&dpr=1"
+
+            ],
+            [
+                'name'=>'LG mobile',
+                "price"=>'10,000',
+                "description"=>'this is the lg 8gb mobile phone with advanced featurs and sleek design',
+                "category"=>'mobile',
+                "gallery"=>"https://images.pexels.com/photos/15315956/pexels-photo-15315956.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=500&w=1400&dpr=1"
+
+            ],
+            [
+                'name'=>'LG mobile',
+                "price"=>'10,000',
+                "description"=>'this is the lg 8gb mobile phone with advanced featurs and sleek design',
+                "category"=>'mobile',
+                "gallery"=>"https://images.pexels.com/photos/15315956/pexels-photo-15315956.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=500&w=1400&dpr=1"
+
+            ]
+        ]);
     }
 }
